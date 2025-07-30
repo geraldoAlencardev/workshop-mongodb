ff651fe283534631dce05bbde2d26a3fb1f97843
Geraldo Alencar <geraldoalencardev@gmail.com>
2025-07-30 00:17:59 -0300

    Crud Operations

--- src/main/java/gera/alencar/workshopmongodb/resources/UserController.java
+++ src/main/java/gera/alencar/workshopmongodb/resources/UserController.java
@@ -5,11 +5,10 @@ import gera.alencar.workshopmongodb.dto.UserDTO;
 import gera.alencar.workshopmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,16 +19,39 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(method= RequestMethod.GET)
-    public ResponseEntity<List<UserDTO>> findAll(){
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> users = userService.findAll();
         List<UserDTO> userDTOs = users.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
         return ResponseEntity.ok().body(userDTOs);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<UserDTO> FindById(@PathVariable String id){
+    public ResponseEntity<UserDTO> FindById(@PathVariable String id) {
         User user = userService.findById(id);
         return ResponseEntity.ok().body(new UserDTO(user));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {
+        User user = userService.fromDTO(userDTO);
+        user = userService.insert(user);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+    public ResponseEntity<Void> DeleteById(@PathVariable String id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody UserDTO userDTO, @PathVariable String id) {
+        User user = userService.fromDTO(userDTO);
+        user.setId(id);
+        userService.update(user);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/gera/alencar/workshopmongodb/services/UserService.java
+++ src/main/java/gera/alencar/workshopmongodb/services/UserService.java
@@ -1,6 +1,7 @@
 package gera.alencar.workshopmongodb.services;
 
 import gera.alencar.workshopmongodb.domain.User;
+import gera.alencar.workshopmongodb.dto.UserDTO;
 import gera.alencar.workshopmongodb.repository.UserRepository;
 import gera.alencar.workshopmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,29 @@ public class UserService {
     public User findById(String id) {
         Optional<User> user = userRepository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+    }
+
+    public User insert(User user) {
+        return userRepository.insert(user);
+    }
+
+    public void delete(String id) {
+        findById(id);
+        userRepository.deleteById(id);
+    }
+
+    public User update(User user) {
+        User newUser = findById(user.getId());
+        updateData(newUser, user);
+        return userRepository.save(newUser);
+    }
+
+    public void updateData(User newUser, User oldUser) {
+        newUser.setName(oldUser.getName());
+        newUser.setEmail(oldUser.getEmail());
+    }
+
+    public User fromDTO(UserDTO userDTO) {
+        return new User(null, userDTO.getName(), userDTO.getEmail());
     }
 }
